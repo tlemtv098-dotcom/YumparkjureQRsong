c813859c9bf5e7b6db35784a99ec903fe76d864e
tlemtv098-dotcom <tlemtv098-dotcom@users.noreply.github.com>
2026-09-02 02:52:32 +0700

    fix: bump SW cache v1->v2 to clear cached player.html with widget_referrer on iPad

--- music/static/music/sw.js
+++ music/static/music/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "yum-juke-v1";
+const CACHE_NAME = "yum-juke-v2";
 const SHELL = ["/", "/request/", "/static/music/img/logo.jpg", "/static/music/manifest.json"];
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(SHELL)).catch(()=>{}));
